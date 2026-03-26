0dbaa451a2ab4aed880eff4d359d0a56d3448dea
CormeraisQuentin <quentin.cormerais@etudiant.univ-rennes.fr>
2026-03-26 10:07:09 +0100

    Ajout des toutes les routes et fonctions. Pas de pagination.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Cette API permet de gérer des environnement utiles au déploiement d'applicatio
 
 ## Endpoints
 
-```POST /envs``` : Création d’un environnement  
-```GET /envs``` : Obtenir la liste des environnements   
-```GET /envs/{envId}``` : Récupérer les informations d’un environnement  
-```PUT /envs/{envId}``` : Modification d’un environnement  
-```DELETE /envs/{envId}``` : Supprimer un environnement   
+```POST /environments``` : Création d’un environnement  
+```GET /environments``` : Obtenir la liste des environnements   
+```GET /environments/{environmentId}``` : Récupérer les informations d’un environnement  
+```PATCH /environments/{environmentId}``` : Modification d’un environnement  
+```DELETE /environments/{environmentId}``` : Supprimer un environnement   
 
 ## Format de donnée
 
@@ -16,9 +16,15 @@ Voici le format de donnée correspondant à l'API Gestion d'Environnements.
 
 ```json
 {
-  "id": 1,
-  "name": "production",
-  "description": "Environnement live",
-  "created_at": "2024-01-10T12:00:00Z"
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "name": "staging-eu-west",
+      "type": "development",
+      "description": "Good description about environments !",
+      "url": "https://example.com/",
+      "active": true,
+      "createdAt": "2026-03-25T12:36:59.455Z",
+      "updatedAt": "2026-03-25T12:36:59.455Z"
+    }
 }
 ```
---
+++ app/app.py
@@ -0,0 +1,110 @@
+from flask import Flask, jsonify, request
+import uuid
+import json
+from datetime import datetime, timezone
+
+def load_env():                                                   # Fonction qui charge les environnements depuis le fichier d'environnements
+    with open("../data/envs.json", "r") as f:
+        return json.load(f)
+    
+def save_env(envs):                                                # Fonction qui sauvegarde les environnements dans le fichier d'environnements
+    with open("../data/envs.json", "w") as f:
+        json.dump(envs, f ,indent=4)
+
+app = Flask(__name__)
+
+@app.route("/environments", methods=["POST"])
+def create_env():
+    
+    user_data = request.get_json()
+
+
+    env_url = user_data.get("url", "")
+    env_description = user_data.get("description", "")
+    env_active = user_data.get("active", True)
+
+    if not "name" in user_data or user_data["name"] == None:
+        return jsonify({"error": "name is required"}), 400
+    env_name = user_data.get("name")
+
+    if not "type" in user_data or user_data["type"] == None:
+        
+        return jsonify({"error": "type is required"}), 400
+    env_type = user_data.get("type")
+        
+
+    envs = load_env()
+
+    env_created_at = datetime.now(timezone.utc).isoformat()
+    
+    uuid_id = str(uuid.uuid4())
+
+    new_env = {                                                   # On créer un nouvel environnement avec le dernier ID + 1 et les donnée entrée par l'utilisateur
+        "id": uuid_id,
+        "name": env_name,
+        "type": env_type,
+        "description": env_description,
+        "url": env_url,
+        "active": env_active,
+        "created_at": env_created_at,
+        "updatedAt": env_created_at
+    }
+
+    envs[uuid_id] = new_env                                          # On ajoute cet environnement à la fin de liste (qui correspond au fichier)
+
+    save_env(envs)
+
+    return jsonify(new_env), 200
+
+@app.route("/environments", methods=["GET"])
+def list_env():
+
+    envs = load_env()                                              # On charge le fichier des environnements
+    return jsonify(envs), 200
+
+@app.route("/environments/<id>", methods=["GET"])
+def detail_env(id):
+    envs = load_env()
+
+    env = envs.get(id)
+
+    return jsonify(env), 200
+
+@app.route("/environments/<id>", methods=["PATCH"])
+def update_env(id):
+
+    user_data = request.get_json()
+
+    envs = load_env()
+
+    env = envs.get(id)
+
+    env_updated_at = datetime.now(timezone.utc).isoformat()
+
+    elems = ["name", "description", "type", "url", "active"]
+
+    for elem in elems:
+        if elem in user_data:
+            env[elem] = user_data[elem]
+    env["updatedAt"] = env_updated_at
+
+    envs[id] = env
+
+    save_env(envs)
+
+    return jsonify(envs), 200
+
+@app.route("/environments/<id>", methods=["DELETE"])
+def del_env(id):
+
+    envs = load_env()
+
+    del envs[id]
+
+    save_env(envs)
+
+    return jsonify({"success": True,"message": "Ressource supprimée avec succès","id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"}), 200
+
+
+if __name__ == '__main__':                                          # On définit les paramètres de notre programme, comme l'adresse qu'il utilise et le port utilisé
+    app.run(host="0.0.0.0", port=5002)
