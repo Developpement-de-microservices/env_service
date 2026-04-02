bc25fb308f51ab7ecd440c0c69379c5e76d013f1
CormeraisQuentin <quentin.cormerais@etudiant.univ-rennes.fr>
2026-04-02 14:02:19 +0200

    Modidifcation du code pour fonctioner avec MongoDB. Modification du DOckerfile en conséquences.

--- app/app.py
+++ app/app.py
@@ -1,28 +1,18 @@
+import os, requests
 from flask import Flask, jsonify, request
-import requests
 from flask_cors import CORS
-import uuid
-import json
 from datetime import datetime, timezone
-
-def load_env():                                                   # Fonction qui charge les environnements depuis le fichier d'environnements
-    with open("./data/envs.json", "r") as f:
-        return json.load(f)
-    
-def save_env(envs):                                                # Fonction qui sauvegarde les environnements dans le fichier d'environnements
-    with open("./data/envs.json", "w") as f:
-        json.dump(envs, f ,indent=4)
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/environments/health", methods=["GET"])
-def heath_env():
-    return jsonify({"status": "ok","service": "Environnements","timestamp": datetime.now(timezone.utc).isoformat()}), 200
+client = MongoClient(os.environ["MONGO_URI"])
+db = client.env_db
+env_col = db.env
 
-@app.route("/environments", methods=["POST"])
-def create_env():
-
+def authentification():
     token = request.headers.get("Authorization", "").replace("Bearer ", "")
     try:
         headers = {"Authorization": f"Bearer {token}"}
@@ -31,6 +21,18 @@ def create_env():
             return jsonify({"error": "Not authorized"}), 401
     except requests.RequestException:
         return jsonify({"error": "Unable to check token, check /auth API"}), 401
+
+
+@app.route("/environments/health", methods=["GET"])
+def heath_env():
+    return jsonify({"status": "ok","service": "Environnements","timestamp": datetime.now(timezone.utc).isoformat()}), 200
+
+@app.route("/environments", methods=["POST"])
+def create_env():
+
+    auth_check = authentification()
+    if auth_check: 
+        return auth_check
     
     user_data = request.get_json()
 
@@ -47,16 +49,11 @@ def create_env():
         
         return jsonify({"error": "type is required"}), 400
     env_type = user_data.get("type")
-        
-
-    envs = load_env()
 
     env_created_at = datetime.now(timezone.utc).isoformat()
-    
-    uuid_id = str(uuid.uuid4())
 
-    new_env = {                                                   # On créer un nouvel environnement avec le dernier ID + 1 et les donnée entrée par l'utilisateur
-        "id": uuid_id,
+
+    new_env = {
         "name": env_name,
         "type": env_type,
         "description": env_description,
@@ -66,100 +63,88 @@ def create_env():
         "updatedAt": env_created_at
     }
 
-    envs[uuid_id] = new_env                                          # On ajoute cet environnement à la fin de liste (qui correspond au fichier)
-
-    save_env(envs)
+    result = env_col.insert_one(new_env.copy())
+    new_env["id"] = str(result.inserted_id)
+    del new_env["_id"]
 
     return jsonify(new_env), 201
 
 @app.route("/environments", methods=["GET"])
 def list_env():
 
-    token = request.headers.get("Authorization", "").replace("Bearer ", "")
-    try:
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post("http://proxy/auth/verify", headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
-    except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
+    auth_check = authentification()
+    if auth_check: 
+        return auth_check
+    
+    is_empty = env_col.find_one()
 
-    envs = load_env()                                              # On charge le fichier des environnements
+    if is_empty is None:
+        return jsonify({"error":"La collection est vide"}), 400 
+
+    envs = []
+    for env in env_col.find():
+        env["id"] = str(env["_id"]) #transforme en id de l'objet mongo
+        del env["_id"]
+        envs.append(env)
+
+
     return jsonify(envs), 200
 
 @app.route("/environments/<id>", methods=["GET"])
 def detail_env(id):
 
-    token = request.headers.get("Authorization", "").replace("Bearer ", "")
+    auth_check = authentification()
+    if auth_check: 
+        return auth_check
+
     try:
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post("http://proxy/auth/verify", headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
-    except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
+        env = env_col.find_one({"_id": ObjectId(id)}) #conversion en type mongoDB pour son ID
+    except:
+        return jsonify({"message": "Invalid event ID format"}), 400
 
-    envs = load_env()
-
-    env = envs.get(id)
-
-    return jsonify(env), 200
+    if env:
+        env["id"] = str(env["_id"])
+        del env["_id"]
+        return jsonify(env), 200
+    
+    return jsonify({"message": "Event not found"}), 404
 
 @app.route("/environments/<id>", methods=["PATCH"])
 def update_env(id):
 
-    token = request.headers.get("Authorization", "").replace("Bearer ", "")
-    try:
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post("http://proxy/auth/verify", headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
-    except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
+    auth_check = authentification()
+    if auth_check: 
+        return auth_check
 
     user_data = request.get_json()
 
-    envs = load_env()
+    user_data["updatedAt"] = datetime.now(timezone.utc).isoformat()
 
-    env = envs.get(id)
+    env = env_col.find_one({"_id": id})
 
-    env_updated_at = datetime.now(timezone.utc).isoformat()
+    if not env:
+        return jsonify({"error":"Aucun environnement correspondant pour cet ID"}), 400
 
-    elems = ["name", "description", "type", "url", "active"]
+    env_col.update_one({"id": id}, {"$set": user_data})
 
-    for elem in elems:
-        if elem in user_data:
-            env[elem] = user_data[elem]
-    env["updatedAt"] = env_updated_at
 
-    envs[id] = env
-
-    save_env(envs)
-
-    return jsonify(envs), 200
+    return jsonify(env_col.find_one({"id": id})), 200
 
 @app.route("/environments/<id>", methods=["DELETE"])
 def del_env(id):
 
-    token = request.headers.get("Authorization", "").replace("Bearer ", "")
-    try:
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.post("http://proxy/auth/verify", headers=headers)
-        if response.status_code != 200:
-            return jsonify({"error": "Not authorized"}), 401
-    except requests.RequestException:
-        return jsonify({"error": "Unable to check token, check /auth API"}), 401
+    auth_check = authentification()
+    if auth_check: 
+        return auth_check
 
-    envs = load_env()
+    env = env_col.find_one({"id": id})
 
-    if id in envs :
-        del envs[id]
-    else : 
-        return jsonify({"error":"ID introuvable"}), 400
+    if not env:
+        return jsonify({"error":"Aucun environnement correspondant pour cet ID"}), 400
 
-    save_env(envs)
+    env_col.delete_one({"id": id})
 
-    return jsonify({"success": True,"message": "Ressource supprimée avec succès","id": "3fa85f64-5717-4562-b3fc-2c963f66afa6"}), 200
+    return jsonify({"success": True,"message": "Ressource supprimée avec succès","id": id}), 200
 
 
 if __name__ == '__main__':                                          # On définit les paramètres de notre programme, comme l'adresse qu'il utilise et le port utilisé
